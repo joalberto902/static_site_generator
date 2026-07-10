dc69e0846992d66556dc116ede5f1030a45ebf81
joalberto902 <joalberto902@gmail.com>
2026-07-10 20:41:57 -0300

    fixing bug

--- src/main.py
+++ src/main.py
@@ -23,7 +23,8 @@ def generate_page(
     title = extract_title(markdown)
 
     template = template.replace("{{ Title }}", title)
-    template = template.replace("{{ Content }}", html_from_md.replace('href="/', f'href="{base_path}').replace('src="/', f'src="{base_path}'))
+    template = template.replace("{{ Content }}", html_from_md)
+    template = template.replace('href="/', f'href="{base_path}').replace('src="/', f'src="{base_path}')
     
 
     if not os.path.exists(dest_path):
