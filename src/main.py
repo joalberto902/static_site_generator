c9980b78c022751192af0528ecb9a0cffb4054fd
joalberto902 <joalberto902@gmail.com>
2026-07-10 20:35:56 -0300

    Code refactoring and fixing deployment bugs

--- src/main.py
+++ src/main.py
@@ -12,12 +12,8 @@ def generate_page(
 )-> None:
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
-    absolute_from_path = os.path.abspath(from_path)
-    absolute_template_path = os.path.abspath(template_path)
-    absolute_dest_path = os.path.abspath(dest_path)
-
-    markdown_file = open(absolute_from_path, "r")
-    template_file = open(absolute_template_path, "r")
+    markdown_file = open(from_path, "r")
+    template_file = open(template_path, "r")
     
     markdown = markdown_file.read()
     html_from_md = markdown_to_html_node(markdown).to_html()
@@ -27,21 +23,20 @@ def generate_page(
     title = extract_title(markdown)
 
     template = template.replace("{{ Title }}", title)
-    template = template.replace("{{ Content }}", html_from_md.replace('href="/', f'href={base_path}').replace('src="/', f'src={base_path}'))
+    template = template.replace("{{ Content }}", html_from_md.replace('href="/', f'href="{base_path}').replace('src="/', f'src="{base_path}'))
     
 
-    if not os.path.exists(absolute_dest_path):
-        dir_name = os.path.dirname(absolute_dest_path)
+    if not os.path.exists(dest_path):
+        dir_name = os.path.dirname(dest_path)
         os.makedirs(dir_name, exist_ok=True)
 
-    with open(absolute_dest_path, "w") as html:
+    with open(dest_path, "w") as html:
         html.write(template)
 
     return None
 
 def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir_path: str, base_path:str) -> None:
-    absolute_dir_path_content = os.path.abspath(dir_path_content)
-    for content in os.listdir(absolute_dir_path_content):
+    for content in os.listdir(dir_path_content):
         path = os.path.join(dir_path_content, content)
         if os.path.isfile(path) and path.endswith(".md"):
             new_path = os.path.join(dest_dir_path, "index.html")
@@ -49,15 +44,18 @@ def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir
             continue
         if os.path.isfile(path):
             continue
-        new_path = os.path.join(os.path.abspath(dest_dir_path), content) 
-        if not os.path.exists(path):
+        new_path = os.path.join(dest_dir_path, content) 
+        if not os.path.exists(new_path):
             os.makedirs(os.path.join(new_path), exist_ok=True)
         
         generate_pages_recursive(path, template_path, new_path, base_path)
 
     return None
 def main() -> int:
-    basepath = sys.argv[1] or "/"
+    try:
+        basepath = sys.argv[1]
+    except:
+        basepath = "/"
     copy_src_to_destination(f"static", f"docs")
     generate_pages_recursive(f"content", f"template.html", f"docs", basepath)
     return 0
